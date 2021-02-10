d615bfe9ff05585272a4c968864605f7970b3a4e
hoseacodes <dominique11h@yahoo.com>
2021-02-10 13:41:51 -0600

    Reversing a string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,29 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    return str.split('').reduce((reversed, char) => char + reversed, '')
+}
 
 module.exports = reverse;
+
+// Solution #1
+// function reverse(str) {
+//     let arr = str.split('')
+//     arr = arr.reverse()
+//     return arr.join('')
+    // or
+    //str.split('').reverse().str.join('') 1 Line
+
+// }
+
+//Solution #2
+// function reverse(str) {
+//     let reversed = ''
+
+//     for (let char of str) {
+//         reversed = char + reversed
+//     }
+
+//     return reversed
+// }
